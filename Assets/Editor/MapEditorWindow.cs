bb397cacb07bd4f1a7e84d32085f362497b79707
is_0130 <hw18a007@oecu.jp>
2020-05-06 17:38:24 +0900

    Windowを表示するようにした

---
+++ Assets/Editor/MapEditorWindow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace MapEditor
+{
+    /// <summary>
+    /// MapEditorを使用するためのWindowを作成する
+    /// @author Shoichi Ikeda
+    /// </summary>
+    public class MapEditorWindow : EditorWindow
+    {
+        /*ユーザーの初期設定*/
+        Object dataDirectory;  //! 使用するオブジェクトが入っているディレクトリ
+        Object outputEmptyObject; //! 作成したマップデータを保管するオブジェクト
+        Vector2 mapSize = new Vector2(10, 10); //! マップサイズを保管
+        Vector3 partsSize = new Vector3(1, 1, 1); //!使用するオブジェクトのサイズを予め記述し、サイズの成型を行う
+
+        /*Developer Settings*/
+        const string WINDOW_NAME = "Map Editor"; //! タブに表示される名前
+        const int TITLE_FONT_SIZE = 32; //! タイトルに使用するフォントサイズ
+        const int HEADER_FONT_SIZE = 20; //! ヘッダーに使用するフォントサイズ
+
+        [MenuItem("Window/"+WINDOW_NAME)]
+        private static void Create()
+        {
+            GetWindow<MapEditorWindow>(WINDOW_NAME);
+        }
+
+        private void OnGUI()
+        {
+
+        }
+    }
+}

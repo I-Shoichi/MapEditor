ae736de926e31f5789e396a521c69a2d49c43db5
I Shoichi <hw18a007@oecu.jp>
2020-06-11 21:48:35 +0900

    バケツ機能追加（空白未対応）

---
+++ Assets/Editor/MapEditor/Bucket.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapEditor
+{
+    /// <summary>
+    /// バケツツール
+    /// @author Shoichi Ikeda
+    /// </summary>
+    public class Bucket : MonoBehaviour
+    {
+        GridCell[,] cells;       //! セルデータ
+        GameObject searchObject; //! 探索対象
+        GameObject drawObject;   //! 塗るオブジェクト
+        Color drawColor;         //! 塗る色
+
+        //コンストラクタ
+        public Bucket(GridCell[,] inputCells, GameObject searchObject, GameObject drawObject, Color color)
+        {
+            this.cells = inputCells;
+            this.searchObject = searchObject;
+            this.drawObject = drawObject;
+            this.drawColor = color;
+        }
+
+        public void Search(int x, int y)
+        {
+            cells[x, y].InputEvent(MouseEvents.paint, drawColor, drawObject);
+
+            //Top
+            if(HasObject(x, y - 1))
+            {
+                Search(x, y - 1);
+            }
+
+            //Left
+            if(HasObject(x - 1, y))
+            {
+                Search(x - 1, y);
+            }
+
+            //Right
+            if(HasObject(x + 1, y))
+            {
+                Search(x + 1, y);
+            }
+
+            //Bottom
+            if(HasObject(x, y + 1))
+            {
+                Search(x, y + 1);
+            }
+        }
+
+        /// <summary>
+        /// オブジェクトを持っているかを判定する
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private bool HasObject(int x, int y)
+        {
+            if(cells[x, y].cellObject == null)
+            {
+                return searchObject == null;
+            }
+
+            return searchObject.Equals(cells[x, y].cellObject);
+        }
+    }
+}
